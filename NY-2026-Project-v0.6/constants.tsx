47fd0b72af16fe54a2418e5a05f8624691a5ac68
The-Witness-7879 <787904692@qq.com>
2026-01-01 20:18:54 +0800

    Update constants.tsx

--- NY-2026-Project-v0.6/constants.tsx
+++ NY-2026-Project-v0.6/constants.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { 
   Sparkles, Repeat, Brain, Swords, Megaphone, Dices, Flame, PartyPopper,
@@ -53,8 +54,8 @@ export const NEW_ACTIVITIES = [
     icon: <Repeat className="w-6 h-6" />, 
     desc: '所有群员均可参与，最先发出有效语音条的视为接龙成功。每次接龙失败就开启新一轮接歌，最后接歌成功次数最多者获胜。',
     award: '接龙王',
-    winnerName: '虚位以待...',
-    winnerAvatar: null
+    winnerName: '氧er氧氧er氧氧氧🐏',
+    winnerAvatar: 'https://ezadrlmlnocyqztyavqd.supabase.co/storage/v1/object/public/profile%20photo/01.jpg'
   },
   { 
     time: '20:00 - 21:00', 
@@ -62,8 +63,8 @@ export const NEW_ACTIVITIES = [
     icon: <Brain className="w-6 h-6" />, 
     desc: '一位群员出题，其余群员根据题目猜歌名。出题形式包括但不限于文字描述、语音、画图和行为艺术等。时间结束时，猜对次数最多者获胜。',
     award: '读心王',
-    winnerName: '虚位以待...',
-    winnerAvatar: null
+    winnerName: '氧er氧氧er氧氧氧🐏',
+    winnerAvatar: 'https://ezadrlmlnocyqztyavqd.supabase.co/storage/v1/object/public/profile%20photo/01.jpg'
   },
   { 
     time: '21:00 - 21:30', 
@@ -71,8 +72,8 @@ export const NEW_ACTIVITIES = [
     icon: <Swords className="w-6 h-6" />, 
     desc: '由群主率先发语音设擂，任意群员都可以发起挑战（最先发出前3条有效语音视为挑战者），其余群员作为评委根据其表现（无标准，唱功优秀或搞笑异常都可以）打分。得分最高者成为新擂主，接受接下来的攻擂。时间结束时，仍站在擂台上的群友获胜。',
     award: '擂歌王',
-    winnerName: '虚位以待...',
-    winnerAvatar: null
+    winnerName: '氧er氧氧er氧氧氧🐏',
+    winnerAvatar: 'https://ezadrlmlnocyqztyavqd.supabase.co/storage/v1/object/public/profile%20photo/01.jpg'
   },
   { 
     time: '21:30 - 22:00', 
@@ -80,8 +81,8 @@ export const NEW_ACTIVITIES = [
     icon: <Megaphone className="w-6 h-6" />,
     desc: '由群主率先开始出题：用方言演唱或朗读指定片段，其他群员模仿（最先发出前3条有效语音视为有效模仿），未能有效模仿的群友化身评委，为模范者打分，每轮得分最高者可以出下一题。时间结束时，得分最高的群友获胜。',
     award: '乡音王',
-    winnerName: '虚位以待...',
-    winnerAvatar: null
+    winnerName: '不孤独的游标卡尺',
+    winnerAvatar: 'https://ezadrlmlnocyqztyavqd.supabase.co/storage/v1/object/public/profile%20photo/02.jpg'
   },
   {
     time: '22:00 - 23:00',
@@ -89,24 +90,20 @@ export const NEW_ACTIVITIES = [
     icon: <Dices className="w-6 h-6" />,
     desc: '由群主率先向任意群员发起挑战，内容不限（对歌、骰子、比等级等等均可），群员接受挑战视为挑战成立。胜利者可以要求失败者选择执行真心话/大冒险，执行完毕后失败者成为新的挑战者。最后累计失败次数最多者获得胜利。',
     award: '冒险王',
-    winnerName: '虚位以待...',
-    winnerAvatar: null
+    winnerName: '咩咩',
+    winnerAvatar: 'https://ezadrlmlnocyqztyavqd.supabase.co/storage/v1/object/public/profile%20photo/03.jpg'
   },
   {
     time: '23:00 - 00:00',
     title: '自由时间：绝凶狂欢',
     icon: <Flame className="w-6 h-6" />,
-    desc: '麦序全开，用热情的自由时间、连续不断的歌声迎接2026年跨年时刻的到来吧！',
-    award: null
+    desc: '麦序全开，用热情的自由时间、连续不断的歌声迎接2026年跨年时刻的到来吧！'
   },
   {
     time: '00:00+',
     title: '跨年时刻：未来进化',
     icon: <PartyPopper className="w-6 h-6" />,
-    desc: '跨年钟声敲响！宣布各小游戏比赛结果 and 抽奖结果并予公示，正式跨入新的一年！',
-    award: '年度锦鲤',
-    winnerName: '虚位以待...',
-    winnerAvatar: null
+    desc: '跨年钟声敲响！宣布各小游戏比赛结果 and 抽奖结果并予公示，正式跨入新的一年！'
   }
 ];
 
